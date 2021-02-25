b3a9f48c1b858c42a852754261b432986bcf39b1
JoeM <joe.moloney496@gmail.com>
2021-02-25 15:54:30 +0000

    The Toobular Scrolls - Online [Tutorial]

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = UnityEngine.Input.GetAxis("Horizontal");
+            float moveVertical = UnityEngine.Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+}
